cd3f7aeba13e47ef2868a9d93d3dca7a2761b8bf
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-07-04 17:37:37 -0300

    Extract confirmation component

---
+++ src/components/comment-confirmation.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class CommentConfirmation extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showConfirm: false
+    };
+  }
+
+  render() {
+
+    let confirmNode;
+
+    if (this.state.showConfirm) {
+      return (
+        <span>
+          <a href="" onClick={this._confirmAction.bind(this)}>Yes </a> - or - <a href="" onClick={this._toggleConfirmMessage.bind(this)}> No</a>
+        </span>
+      );
+    } else {
+      confirmNode = <a href="" onClick={this._toggleConfirmMessage.bind(this)}>{this.props.children}</a>;
+    }
+
+    return (
+      <span>
+        {confirmNode}
+      </span>
+    );
+  }
+
+  _toggleConfirmMessage(event) {
+    event.preventDefault();
+
+    this.setState({
+      showConfirm: !this.state.showConfirm
+    });
+
+  }
+
+  _confirmAction(event) {
+    event.preventDefault();
+    this.props.onConfirm();
+  }
+}
