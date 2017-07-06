f7137042225526e906a8cfb94a1f62221acba134
Tulio Casagrande <tcasagra@thoughtworks.com>
2017-07-06 09:06:04 -0300

    Remove jQuery from the project

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import jQuery from 'jquery'
 
 import CommentBox from './components/comment-box'
 
-jQuery(function() {
-  ReactDOM.render(
-    <CommentBox />,
-    document.getElementById('comment-box')
-  );
-})
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('comment-box')
+);
