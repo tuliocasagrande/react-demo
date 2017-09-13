bfdee9b7e7a932812859b55e9432032b588ea038
Tulio Casagrande <tuliocasagrande@gmail.com>
2017-09-13 16:01:35 -0300

    Change character count style

--- src/components/comment-form.js
+++ src/components/comment-form.js
@@ -20,7 +20,7 @@ export default class CommentForm extends React.Component {
           <input placeholder="Name:" ref={c => this._author = c} />
           <textarea placeholder="Comment:" ref={c => this._body = c} onChange={this._getCharacterCount}></textarea>
         </div>
-        <p>{this.state.characters} characters</p>
+        <p className="character-count">{this.state.characters} characters</p>
         <div className="comment-form-actions">
           <button type="submit">
             Post comment
