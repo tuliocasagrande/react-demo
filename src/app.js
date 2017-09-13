f269882f6280ec7ffbaa845d8124beb0e4cd0f78
Tulio Casagrande <tuliocasagrande@gmail.com>
2017-09-13 17:11:00 -0300

    Add hashHistory to parent Route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Redirect } from 'react-router';
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import Layout from './layout/layout'
 import BlogPage from './pages/blog'
@@ -8,7 +8,7 @@ import PicturePage from './pages/picture'
 import VideoPage from './pages/video'
 
 const app = (
-  <Router>
+  <Router history={hashHistory}>
     <Redirect from="/" to="/blog" />
     <Route path="/" component={Layout}>
       <Route path="blog" component={BlogPage} />
